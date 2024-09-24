529f9ee725206c011c96692227cf6c418ca38973
Pedro Henrique Silva Sousa <pedrohenriquesousa1523@gmail.com>
2024-09-24 14:30:10 -0300

    Atualização

--- routes/produto.py
+++ routes/produto.py
@@ -4,7 +4,7 @@ from models import Produto
 
 produto_route = Blueprint('produto', __name__)
 
-@produto_route.route('/listar_Produtos')
+@produto_route.route('/listar_produtos')
 def listagem_produtos():
     lista_produto = Produto.query.all()
     return render_template('listar.html', lista=lista_produto)
@@ -13,28 +13,35 @@ def listagem_produtos():
 def cadastrar_produto():
     return render_template('cadastrar.html')
 
-
-
 @produto_route.route('/cadastrar_enviar', methods=['POST'])
 def cadastrar_enviar_produto():
     nome = request.form['nome']
     categoria = request.form['categoria']
-    quantidade = request.form['quantidade']
-    valor = request.form['valor']
+    
+    
+    quantidade = request.form['quantidade'] or 0  
+    preco = request.form['preco'] or 0.0  
+    
+    try:
+        quantidade = int(quantidade) 
+        preco = float(preco)  
+    except ValueError:
+        flash('Quantidade e preço devem ser valores numéricos!', 'danger')
+        return redirect(url_for('produto.cadastrar_produto'))
 
-    p = Produto(nome, categoria, quantidade, valor)
+    p = Produto(nome=nome, categoria=categoria, quantidade=quantidade, preco=preco)
 
     db.session.add(p)
     db.session.commit()
     
     flash('Produto cadastrado com sucesso!', 'success')
-    return redirect(url_for('produto.listagem_produtos'))
+    
+    # Redirecionar de volta para a página de cadastro
+    return redirect(url_for('produto.cadastrar_produto'))
 
 @produto_route.route('/editar/<int:id_produto>')
 def editar(id_produto):
-
     p = Produto.query.get(id_produto)
-
     return render_template('editar.html', dados_produto=p)
 
 @produto_route.route('/editar_enviar', methods=['POST'])
@@ -42,14 +49,23 @@ def editar_enviar():
     id_produto = request.form['id_produto']
     nome = request.form['nome']
     categoria = request.form['categoria']
-    quantidade = request.form['quantidade']
-    valor = request.form['valor']
+    
+    # Validação para quantidade e preço
+    quantidade = request.form['quantidade'] or 0
+    preco = request.form['preco'] or 0.0
+    
+    try:
+        quantidade = int(quantidade)
+        preco = float(preco)
+    except ValueError:
+        flash('Quantidade e preço devem ser valores numéricos!', 'danger')
+        return redirect(url_for('produto.editar', id_produto=id_produto))
 
     p = Produto.query.get(id_produto)
     p.nome = nome
     p.categoria = categoria
     p.quantidade = quantidade
-    p.valor = valor
+    p.preco = preco
 
     db.session.add(p)
     db.session.commit()
@@ -64,5 +80,5 @@ def excluir(id_produto):
     db.session.delete(p)
     db.session.commit()
 
-    flash('Produto excluído com sucesso!','danger')
+    flash('Produto excluído com sucesso!', 'danger')
     return redirect(url_for('produto.listagem_produtos'))
